9dc08d10037ce8391c26d12bc9e678d9d7f51722
Ian <ian@oneliveweb.com>
2025-12-29 09:55:17 -0600

    add a new module !

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from tf_core.web.webserver import BaseWebServer
 
 from thoughtframe.sensor.SensorMeshManager import SensorMeshManager
 from thoughtframe.sensor.mesh_config import THOUGHTFRAME_APP_CONFIG
+from thoughtframe.sensor.web.sensor_module import SensorModule
 
 
 async def heartbeat():
@@ -22,14 +23,15 @@ async def main():
     configure(THOUGHTFRAME_APP_CONFIG)
     ##Add out own 
     thoughtframe.manager.register("sensormeshmanager",lambda: SensorMeshManager(thoughtframe.manager))
-    
+    thoughtframe.manager.register("SensorModule",lambda: SensorModule())
+
     connection_service = thoughtframe.get("connection")
    
     sensormanager  = thoughtframe.get("sensormeshmanager")
     sensormanager.start()
     
     web : BaseWebServer = thoughtframe.get("web")
-    
+    await web.start()
 
     
     await asyncio.Event().wait()
